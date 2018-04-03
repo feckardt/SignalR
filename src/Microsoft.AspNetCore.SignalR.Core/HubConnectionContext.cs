f12d2257ffc79a1de3d047c09ca6a7cea1c12762
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-04-03 16:35:49 -0700

    pr feedback: just make it public

--- src/Microsoft.AspNetCore.SignalR.Core/HubConnectionContext.cs
+++ src/Microsoft.AspNetCore.SignalR.Core/HubConnectionContext.cs
@@ -70,7 +70,7 @@ namespace Microsoft.AspNetCore.SignalR
 
         public virtual PipeReader Input => _connectionContext.Transport.Input;
 
-        public string UserIdentifier { get; internal set; }
+        public string UserIdentifier { get; set; }
 
         internal virtual IHubProtocol Protocol { get; set; }
 
